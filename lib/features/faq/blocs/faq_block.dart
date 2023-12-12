1f4ab509f689e1b0a3e66e62e6a58c6dcb055409
Artem Belkov <bestK1ngArthur@aol.com>
2023-12-12 22:36:39 +0400

    Add cacheable to bloc

--- lib/features/faq/blocs/faq_block.dart
+++ lib/features/faq/blocs/faq_block.dart
@@ -1,9 +1,10 @@
 import 'package:breathe_with_me/features/faq/models/faq_state.dart';
 import 'package:breathe_with_me/repositories/faq_questions_repository.dart';
+import 'package:breathe_with_me/utils/cacheable_bloc/cacheable_bloc.dart';
 import 'package:breathe_with_me/utils/content_language.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-final class FaqBloc extends BlocBase<FaqState> {
+final class FaqBloc extends CacheableBloc<FaqState> {
   final FaqQuestionsRepository _questionsRepository;
 
   FaqBloc(this._questionsRepository) : super(const FaqState.loading());
@@ -13,4 +14,10 @@ final class FaqBloc extends BlocBase<FaqState> {
     final questions = await _questionsRepository.getQuestions(language);
     emit(FaqState.data(questions));
   }
+
+  @override
+  FaqState fromJson(Map<String, dynamic> json) => FaqState.fromJson(json);
+
+  @override
+  Map<String, dynamic> toJson(FaqState state) => state.toJson();
 }
