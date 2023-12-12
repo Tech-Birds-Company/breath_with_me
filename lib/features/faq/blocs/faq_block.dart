806d9002c1004e42d4f07b67500e74a73beddf8e
Artem Belkov <bestK1ngArthur@aol.com>
2023-12-12 22:38:43 +0400

    Remove unused import

--- lib/features/faq/blocs/faq_block.dart
+++ lib/features/faq/blocs/faq_block.dart
@@ -2,7 +2,6 @@ import 'package:breathe_with_me/features/faq/models/faq_state.dart';
 import 'package:breathe_with_me/repositories/faq_questions_repository.dart';
 import 'package:breathe_with_me/utils/cacheable_bloc/cacheable_bloc.dart';
 import 'package:breathe_with_me/utils/content_language.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 final class FaqBloc extends CacheableBloc<FaqState> {
   final FaqQuestionsRepository _questionsRepository;
