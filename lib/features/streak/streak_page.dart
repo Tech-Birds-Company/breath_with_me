acf43c5df80fb9b329cc376a6a5a22dba707f0d5
Artem Belkov <bestK1ngArthur@aol.com>
2023-12-22 21:22:31 +0400

    Throw bloc as param

--- lib/features/streak/streak_page.dart
+++ lib/features/streak/streak_page.dart
@@ -1,20 +1,21 @@
-import 'package:breathe_with_me/di/di.dart';
 import 'package:breathe_with_me/features/streak/blocs/streak_bloc.dart';
 import 'package:breathe_with_me/features/streak/models/streak_state.dart';
 import 'package:breathe_with_me/features/streak/widgets/streak_list.dart';
 import 'package:breathe_with_me/theme/bwm_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class StreakPage extends ConsumerWidget {
-  const StreakPage({super.key});
+class StreakPage extends StatelessWidget {
+  final StreakBloc bloc;
+
+  const StreakPage({
+    required this.bloc,
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final theme = Theme.of(context).extension<BWMTheme>()!;
-    final bloc = ref.read(Di.shared.bloc.streak);
-
     return Scaffold(
       backgroundColor: theme.primaryBackground,
       body: SafeArea(
