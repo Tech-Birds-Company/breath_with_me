1b7a7bbebc205a2728cc80783d3863f70fec8c67
Vasilii Dmitriev <vasily.dmitriev98@gmail.com>
2024-01-21 21:44:21 +0100

    setup crashlytics

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -21,7 +21,9 @@ import 'package:breathe_with_me/utils/cacheable_bloc/isar_bloc_storage.dart';
 import 'package:breathe_with_me/utils/environment.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -106,10 +108,19 @@ Future<ProviderContainer> _setupDependencies({
   return ProviderContainer(overrides: dependencies);
 }
 
+void _setupCrashlytics() {
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+}
+
 Future<void> mainCommon(Environment env) async {
   WidgetsFlutterBinding.ensureInitialized();
   tz.initializeTimeZones();
   await Firebase.initializeApp();
+  _setupCrashlytics();
   await EasyLocalization.ensureInitialized();
 
   await FirebaseRemoteConfig.instance.ensureInitialized();
